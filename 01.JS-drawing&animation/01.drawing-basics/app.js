3a303525f56fc8602087066b82b9f9a58c1f54fd
mameMan <jokingbear01@gmail.com>
2022-02-17 10:06:53 +0900

    making-drawings-with-code

---
+++ 01.JS-drawing&animation/01.drawing-basics/app.js
@@ -0,0 +1,9 @@
+//ellipse(x, y, w, h)
+/*
+x: 0 ~ 400
+y: 0 ~ 400
+*/
+ellipse(200, 200, 300, 300); //face
+ellipse(200, 270, 100, 70); //mouth
+ellipse(150, 150, 50, 50); //left-eye
+ellipse(250, 150, 50, 50); //right-eye
